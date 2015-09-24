70a384661b929ec9f1b45f8ac3b7e7fac44bd99e
jake trower <jakeonaut@gmail.com>
2015-09-24 11:42:01 -0500

    removed outdated named files

--- ex011.rb
+++
@@ -1,9 +0,0 @@
-print "How old are you? "
-#gets takes input, chomp gets rid of linebreak
-age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weight? "
-weight = gets.chomp
-
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
--- ex17_oneline.rb
+++
@@ -1,1 +0,0 @@
-from_file, to_file = ARGV; indata = open(from_file).read; open(to_file, 'w').write(indata);
